1162a5a3aff38a422d11f91736d5aef22eb95576
Lucas Lima <lucasViniLima15@gmail.com>
2023-09-05 15:02:25 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SGPE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

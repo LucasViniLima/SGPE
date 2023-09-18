39e126a818a4d6bbfe327461ae62a577434f314b
Lucas Lima <lucasViniLima15@gmail.com>
2023-09-18 13:06:08 -0300

    adicionando model de produto

---
+++ Models/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace SGPE.Models
+{
+    public class Produto
+    {
+        public long Id { get; set; }
+        public string? Titulo { get; set; }
+        public string? Descricao { get; set; }
+        public DateTime? DataAtualizacao { get; set; }
+        public DateTime DataCriacao = DateTime.Now;
+    }
+}

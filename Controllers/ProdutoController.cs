55ba82f3d0b21768e5ccedc5e2c3dcaa90da6b46
Lucas Lima <lucasViniLima15@gmail.com>
2023-09-26 11:08:18 -0300

    criando controller de produto

---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SGPE.Context;
+using SGPE.Models;
+
+namespace SGPE.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ProdutoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Titulo, Descricao")] Produto produto)
+        {
+            _context.Add(produto);
+            await _context.SaveChangesAsync();
+            return Redirect(nameof(Index));
+        }
+    }
+}
